ed1d21740766880a7ab1f047e55545e3661773d5
milligramme <post@milligramme.cc>
2011-05-10 17:45:06 +0900

    add for illustrator

---
+++ _Illustrator/ai_anchor_infomation.js
@@ -0,0 +1,13 @@
+#target "Illustrator"
+doc = app.documents[0];
+sel = doc.selection;
+
+p_point = sel[0].pathPoints;
+for (var pi=0, piL=p_point.length; pi < piL ; pi++) {
+	$.writeln(p_point[pi].leftDirection);
+	$.writeln(p_point[pi].anchor);
+	$.writeln(p_point[pi].rightDirection);
+	if (p_point[pi].pointType == PointType.CORNER){
+		p_point[pi].pointType = PointType.SMOOTH
+	}
+};
