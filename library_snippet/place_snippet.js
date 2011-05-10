f6a3d52a29a97b2e3412138de695c9844c7841c1
milligramme <post@milligramme.cc>
2011-05-10 16:23:52 +0900

    a

--- library_snippet/place_snippet.js
+++ library_snippet/place_snippet.js
@@ -1,1 +1,4 @@
-x=0y=0var placeditem = app.documents[0].pages[0].place(File("~/Desktop/a.inds"),undefined, undefined, false, false);//place (fileName, placePoint, destinationLayer, showingOptions, autoflowing, withProperties)
+var x = 0;
+var y = 0;
+var placeditem = app.documents[0].pages[0].place(File("~/Desktop/a.inds"),undefined, undefined, false, false);
+//place (fileName, placePoint, destinationLayer, showingOptions, autoflowing, withProperties)
